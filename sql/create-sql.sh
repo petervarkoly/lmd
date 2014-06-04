a034496ccada103a5f4ab7cd411d793ecf6c6cf7
OSS Build Service <build@extis.de>
2014-06-04 06:19:05 +0200

    Do not drop lmd anymore.

--- sql/create-sql.sh
+++ sql/create-sql.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Copyright (c) 2012 Peter Varkoly <peter@varkoly.de> Nürnberg, Germany.  All rights reserved.
+# Copyright (c) 2004-2014 Peter Varkoly <peter@varkoly.de> Nürnberg, Germany.  All rights reserved.
 rcmysql status || rcmysql start
 for i in /usr/share/lmd/sql/*.sql
 do
 	echo "Create lmd sql tabelles from $i"
-	mysql < $i
+	mysql -f < $i
 done
 echo "grant all on lmd.* to lmd@localhost  identified by \"lmd\"" | mysql
