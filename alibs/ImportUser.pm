59612e19c39fb219a094a75188b95a9c6a5a9c82
Peter Varkoly <varkoly@suse.de>
2016-08-18 17:40:32 +0200

    Ticket#2016081810000609 Erweiterung für Import von Benutzern - Schülern am GVRBK Dortmund

--- alibs/ImportUser.pm
+++ alibs/ImportUser.pm
@@ -55,6 +55,8 @@ sub getCapabilities
                 { variable     => [ "resetPW",         [ type => "boolean" ] ] },
                 { variable     => [ "full",            [ type => "boolean" , label=>"This list contains all user" ] ] },
                 { variable     => [ "test",            [ type => "boolean" , label=>"Try only what would happen"] ] },
+                { variable     => [ "allClasses",      [ type => "boolean" , label=>"This list contains all classes", help=>"Classes which are not in the list will be deleted. This parameter has only affect for students." ] ] },
+                { variable     => [ "cleanClassDirs",  [ type => "boolean" , label=>"Clean up the directories of classes"] ] },
                 { variable     => [ "alias",           [ type => "boolean" ] ] },
                 { variable     => [ "mailenabled",     [ type => "popup" ] ] },
 		{ variable     => [ "role",            [ type => "popup" ] ] },
@@ -126,6 +128,8 @@ sub default
 		{ lang         => getLanguages(main::GetSessionValue('lang')) },
 		{ test	       => 1 },
 		{ full	       => 0 },
+                { allClasses   => 0 },
+                { cleanClassDir=> 0 },
 		{ alias	       => 0 },
 		{ mustchange   => 0 },
 		{ resetPW      => 0 },
@@ -173,6 +177,14 @@ sub import
 	{
 		$attributes .= ' --full';
 	}
+        if( $reply->{role} eq 'students' && $reply->{cleanClassDir} )
+        {
+                $attributes .= ' --cleanClassDir';
+        }
+        if( $reply->{role} eq 'students' && $reply->{allClasses} )
+        {
+                $attributes .= ' --allClasses';
+        }
 	if( $reply->{mustchange} )
 	{
 		$attributes .= ' --mustchange';
